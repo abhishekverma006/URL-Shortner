8e18cba70c8bb8d8ed34a9162e27d0e13c24cc71
AbhishekVerma006 <abhishekverma84951@gmail.com>
2025-11-24 16:58:13 +0530

    baseurl change

--- Frontend/src/api/axiosInstance.api.js
+++ Frontend/src/api/axiosInstance.api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: `${process.env.VITE_BASE_URL || 'http://localhost:8000'}`,
+    baseURL: `${process.env.VITE_BASE_URL}`,
     headers: {
         'Content-Type': 'application/json',
     },
